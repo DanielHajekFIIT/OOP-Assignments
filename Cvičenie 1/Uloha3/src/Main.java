1415ce1d4bef44d1ed5f14efa9f94157bc94edc0
DanielHajekFIIT <xhajek@stuba.sk>
2026-02-24 18:04:07 +0100

    Delete Cvičenie 1 directory

--- Cvičenie 1/Uloha3/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String sampleString = "string";
-        //Pri x treba špecifikovať dátový typ
-        for (char x : sampleString.toCharArray()) {
-            System.out.print(x);
-        }
-    }
-}
