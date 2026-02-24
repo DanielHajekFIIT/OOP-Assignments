f5cad45a768384d1b8701da60de94aa40d25cf4d
Daniel Hájek <hajekdaniel100@gmail.com>
2026-02-24 21:13:47 +0100

    Pridaná warrior class

---
+++ assignment-14/task6/default-structure/src/main/java/org/jikvict/tasks/exposed/Warrior.java
@@ -0,0 +1,35 @@
+package org.jikvict.tasks.exposed;
+
+public class Warrior extends GameCharacter {
+    private int armorRating;
+
+    public Warrior(String name, int health, int baseAttackPower, int armorRating) {
+        super(name, health, baseAttackPower);
+        this.armorRating = armorRating;
+    }
+
+    public String getCharacterType() {
+        return "Warrior";
+    }
+
+    public int calculateDamage() {
+        return baseAttackPower + armorRating/2;
+    }
+
+    public void takeDamage(int damage) {
+        super.takeDamage(Math.max(0, damage - armorRating/3));
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + " | Armor:  " + armorRating;
+    }
+
+    public int getArmorRating() {
+        return armorRating;
+    }
+
+    public void setArmorRating(int armorRating) {
+        this.armorRating = armorRating;
+    }
+}
