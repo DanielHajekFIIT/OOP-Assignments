d990635a5ffca1e872ea25054f03c253ef4bf4f8
DanielHajekFIIT <xhajek@stuba.sk>
2026-03-14 13:20:42 +0100

    Delete assignment-16 manual submit directory

--- assignment-16 manual submit/task8/default-structure/src/main/java/org/jikvict/tasks/exposed/CharacterDeadException.java
+++
@@ -1,7 +0,0 @@
-package org.jikvict.tasks.exposed;
-
-public class CharacterDeadException extends GameException {
-    public CharacterDeadException(String message) {
-        super(message);
-    }
-}
--- assignment-16 manual submit/task8/default-structure/src/main/java/org/jikvict/tasks/exposed/InvalidPotionException.java
+++
@@ -1,7 +0,0 @@
-package org.jikvict.tasks.exposed;
-
-public class InvalidPotionException extends GameException {
-    public InvalidPotionException(String message) {
-        super(message);
-    }
-}
